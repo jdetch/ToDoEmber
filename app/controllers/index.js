89fde904b2eb777c38c4a33f957d51c94fbf24c5
Jodi Detch <jodidetch@gmail.com>
2014-09-21 11:25:10 -0400

    App allows users to check off completed tasks

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  setup: function(){
 
-  }.on('init'),
+ completedTasks: Ember.computed.filterBy('model', 'isCompleted', true),
+
+ incompletedTasks: Ember.computed.filterBy('model', 'isCompleted', false),
+
   actions: {
     createTodo: function(){
       var item = Ember.Object.create({
-        title: this.get('newTitle')
+        title: this.get('newTitle'),
+        isCompleted: false
       });
     this.get('model').addObject(item);
     this.set('newTitle', '');
